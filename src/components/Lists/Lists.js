702552a5fb41c375612b501698b9f7534be6a60a
gv4383 <gregory.vargas18@gmail.com>
2018-10-19 18:16:30 -0500

    Pulled new lists from server
    Created a array, prefilled with a couple of test list names

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import List from "./List/List";
 
 class Lists extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lists: []
+    };
+  }
+
+  componentDidMount = () => {
+    axios.get("/api/lists").then(response => {
+      this.setState({ lists: response.data });
+    });
+  };
+
   render() {
+    const { lists } = this.state;
+
+    const displayLists = lists.map((list, i) => {
+      return <p key={i}>{list}</p>;
+    });
+
     return (
       <div>
-        <List />
+        {/* <List /> */}
+        <h2>Lists</h2>
+        {displayLists}
       </div>
     );
   }
