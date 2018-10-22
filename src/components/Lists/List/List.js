7a1ade565bd7fe819c4c90510f3e5c7b07f497f5
gv4383 <gregory.vargas18@gmail.com>
2018-10-21 21:17:11 -0500

    Reworked List component
    Each list is rendered from a separate component called List

--- src/components/Lists/List/List.js
+++ src/components/Lists/List/List.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 class List extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { click, listName, listDescription } = this.props;
+
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h3 onClick={click}>{listName}</h3>
+        <p>{listDescription}</p>
       </div>
     );
   }
