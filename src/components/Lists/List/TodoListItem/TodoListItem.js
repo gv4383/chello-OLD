48077b3c4cebfc0a475089b4d7ecff3c9619aaf3
gv4383 <gregory.vargas18@gmail.com>
2018-11-25 17:48:46 -0600

    Implemented conditional rendering in TodoListItem component
    Clicking on a to-do list item will change the local state in TodoListItem, which will conditionally render an input box, a submit button, and a cancel button.

--- src/components/Lists/List/TodoListItem/TodoListItem.js
+++ src/components/Lists/List/TodoListItem/TodoListItem.js
@@ -3,17 +3,42 @@ import React, { Component } from "react";
 class TodoListItem extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      listItemInput: this.props.listItem,
+      editListItem: false
+    };
   }
+
+  onChangeHandler = event => {
+    this.setState({ listItemInput: event.target.value });
+  };
+
+  editListItemOnClickHandler = () => {
+    const { editListItem } = this.state;
+    this.setState({ editListItem: !editListItem });
+  };
 
   render() {
     const { listItem, deleteOnClickHandler } = this.props;
+    const { listItemInput, editListItem } = this.state;
 
-    return (
-      <div className="todo-list-item">
-        <p>{listItem}</p>
-        <button onClick={deleteOnClickHandler}>X</button>
-      </div>
-    );
+    if (editListItem) {
+      return (
+        <div className="todo-list-item">
+          <input value={listItemInput} onChange={this.onChangeHandler} />
+          <button>Submit</button>
+          <button onClick={this.editListItemOnClickHandler}>Cancel</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="todo-list-item">
+          <p onClick={this.editListItemOnClickHandler}>{listItem}</p>
+          <button onClick={deleteOnClickHandler}>X</button>
+        </div>
+      );
+    }
   }
 }
 
